a29927a1c7d80cafad6e6c7d1504be817796b1a7
John Stechschulte <john.l.stechschulte@gmail.com>
2015-12-30 12:03:36 -0500

    checked pack.py with check.py

--- pack.py
+++ pack.py
@@ -9,15 +9,16 @@ ip = b"\d{1,3}\."*4
 tcpd_re = re.compile(b"(?:\d\d:\d\d:\d\d.\d{6}) IP ("+ip+b")(\d{1,5}) > ("+ip+
     b")(\d{1,5}): (?:tcp|UDP,(?: bad)? length) (\d{1,4})")
 
+t = 0
+z = 0
+tot_sz = 0
+
 with open("data/packed.bin", "wb") as outfile:
   for infile in ("data/equinix-chicago.dirA.20150219-130000.UTC.anon.pcap",
       "data/equinix-chicago.dirA.20150219-130100.UTC.anon.pcap"):
     with subprocess.Popen(shlex.split(
       tcpd_cmd.format(infile)),
       stdout=subprocess.PIPE, bufsize=1) as tcpd:
-      t = 0
-      z = 0
-      tot_sz = 0
       for l in tcpd.stdout:
         if b"ip-proto-" in l: continue
         try: (fm_ip, fm_pt, to_ip, to_pt, sz) = tcpd_re.match(l).groups()
